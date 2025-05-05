ddd120697da88ee2a4ab3879ea1c509eff568317
eli <eliggold@gmail.com>
2025-05-05 10:49:41 -0400

    working with only minor hiccups when updating needle position

--- BOOSTGAUGE.ino
+++ BOOSTGAUGE.ino
@@ -4,7 +4,7 @@
 
 #define ARDUINO_ARCH_RP2040 1 // import proper TFT_eSPI settings
 #define PSI_CONV_FACTOR 0.145038f // PSI/kPa
-#define PSI_SCALE    10
+#define PSI_SCALE       10
 #define PIN_3V3      18
 #define PIN_RX       17
 #define PIN_TX       16
@@ -13,7 +13,7 @@
 #define TFT_ROTATION LV_DISPLAY_ROTATION_90
 #define ELM_TIMEOUT  1000
 
-#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 2)
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES * 2 / 3)
 uint32_t draw_buf1[DRAW_BUF_SIZE / 4];
 uint32_t draw_buf2[DRAW_BUF_SIZE / 4];
 lv_obj_t *psi_scale;
@@ -22,11 +22,13 @@ lv_obj_t *needle;
 lv_display_t *disp;
 
 #define ELM_SER   Serial1
-#define ELM_DEBUG true
+#define ELM_DEBUG false
 ELM327 my_ELM;
-// volatile bool data_ready = false;
-// void serialEvent1() { if (ELM_SER.available()) { data_ready = true; } }
-static const float C_kPa = 17.37; // 289.526918???; // rev/min*°K*s^2 * 1kPa/1000Pa
+
+static const float C_kPa = 17.37; // rev/(min*°K*s^2) * 1kPa/1000Pa
+
+enum State { IDLE, WAITING };
+State OBDstate = IDLE;
 
 typedef struct struct_boost { // IAT * MAF * C / RPM
   float iat = 225.9; //   °K
@@ -43,16 +45,11 @@ volatile uint8_t try_again = 0;
 volatile unsigned long last_time;
 volatile unsigned long atm_time; // check once per minute, maybe 10s
 volatile uint32_t increment = 0;
-  //                    iat     rpm     maf     atm
-const char* pids[] = { "010F", "010C", "0110", "0133" };
+const int PID_COUNT = 4;
+  //                               iat   rpm   maf   atm
+const uint8_t PIDs[PID_COUNT] = { 0x0F, 0x0C, 0x10, 0x33 };
+const char compPIDs[PID_COUNT] = { 'F', 'C', '0', '3' };
 
-bool handle_IAT();
-bool handle_MAF();
-bool handle_RPM();
-bool handle_ATM(); // void handle_BAT();
-bool (*pid_handlers[])() = { handle_IAT, handle_RPM, handle_MAF, handle_ATM };
-
-const int PID_COUNT = sizeof(pids)/sizeof(pids[0]);
 lv_color_t grey = LV_COLOR_MAKE(95,95,95);
 lv_color_t red = LV_COLOR_MAKE(255,19,0);
 lv_color_t main_color;
@@ -69,91 +66,80 @@ void setup() {
   ELM_SER.setTX(PIN_TX);
   ELM_SER.setRX(PIN_RX);
   ELM_SER.begin(38400);
-  if (!my_ELM.begin(ELM_SER, ELM_DEBUG, ELM_TIMEOUT)) { try_again = 5; } // NO ELM327!!!
+  if (!ELM_SER) { while(1); } // do something here if serial port didn't start
+  if (!my_ELM.begin(ELM_SER, ELM_DEBUG, ELM_TIMEOUT)) { try_again = 5; }
 
   main_color = lv_palette_lighten(LV_PALETTE_GREY, 1);
   blue = lv_palette_darken(LV_PALETTE_BLUE,4);
   make_screen_elements();
   make_styles();
+
+  if (try_again > 0) {
+    if (!my_ELM.begin(ELM_SER, ELM_DEBUG, ELM_TIMEOUT)) {
+      if (0 == try_again) {
+        while(1); // No ELM available after multiple tries
+      } else { --try_again; }
+      delay(100);
+    } else { try_again = 0; }
+  }
   last_time = millis();
   atm_time = millis();
 }
 
 void loop() {
-  if (millis() - last_time >= increment || increment > 1000) { // non-blocking way to periodically perform tasks
+  if (millis() - last_time >= increment || increment > 1000) {
     increment = lv_timer_handler();
-    last_time = millis(); }
-    Serial.print("inc ms: ");
-    Serial.println(increment);
-//  }
-  
-  if(pid_handlers[cur_PID]()) {
-    Serial.print("curr PID: ");
-    Serial.println(pids[cur_PID]);
-    if (cur_PID < 2) {
-      cur_PID = ++cur_PID;
-    } else if (millis() > atm_time) {
-      cur_PID = 3;
-      atm_time = millis() + 10000;
-    } else {
-      cur_PID = 0;
-    }
-    if (0 == cur_PID) {
-      calc_angle();
-      lv_tick_inc(millis() - last_time);
-      Serial.print("angle: ");
-      if (boost_data.is_psi) { Serial.println(boost_data.boost_angle);
-      } else { Serial.println(boost_data.boost_angle - 40); }
-    } // lv_tick_inc(millis() - last_time);
-    clearELMBuffer();
+    last_time = millis();
   }
-}
-
-void clearELMBuffer() {
-    unsigned long timeout = millis() + 100; // 100ms timeout
-    while (millis() < timeout && ELM_SER.available()) {
-        ELM_SER.read();
-    }
-}
-
-bool handle_IAT() {
-  float temp = my_ELM.intakeAirTemp();
-  bool ready = 0.0 != temp;
-  if (ready) { boost_data.iat = temp + 273.15f; }
-  return ready;
-}
-
-bool handle_RPM() {
-  float rpm = my_ELM.rpm();
-  bool ready = 0.0 != rpm;
-  if (ready) { boost_data.rpm = rpm; }
-  return ready;
-}
-
-bool handle_MAF() {
-  float maf = my_ELM.mafRate();
-  bool ready = 0.0 != maf;
-  if (ready) { boost_data.maf = maf; }
-  return ready;
-}
-
-bool handle_ATM() {
-  uint8_t atm = my_ELM.absBaroPressure();
-  bool ready = 0 < atm;
-  if (ready) { boost_data.atm = atm; }
-  return ready;
+  switch (OBDstate) {
+    case IDLE:
+      my_ELM.queryPID(1, PIDs[cur_PID], 1);
+      OBDstate = WAITING;
+      break;
+    case WAITING:
+      int8_t readState = my_ELM.get_response();
+      if (ELM_GETTING_MSG != readState && ELM_TIMEOUT != readState) {
+        if (compPIDs[cur_PID] == my_ELM.payload[3]) {
+          uint64_t data = my_ELM.findResponse();
+          switch (PIDs[cur_PID]) {
+            case 0x0F: //iat in Celcius
+              boost_data.iat = data + 233.15f; // + 273.15 (C to K) - 40 (IAT bias)
+              break;
+            case 0x0C: //rpm x 4
+              boost_data.rpm = data / 4.0f;
+              break;
+            case 0x10: //maf (g/s) x 100
+              boost_data.maf = data / 100.0f;
+              break;
+            case 0x33: //atm (kPa)
+              boost_data.atm = (uint8_t)data;
+          }
+          if (cur_PID < 2) {
+            cur_PID = ++cur_PID;
+          } else if (millis() > atm_time) {
+            cur_PID = 3;
+            atm_time = millis() + 10000;
+          } else {
+            cur_PID = 0;
+          }
+        }
+        OBDstate = IDLE;
+        my_ELM.flushInputBuff();
+        if (0 == cur_PID) {
+          calc_angle();
+          lv_tick_inc(millis() - last_time);
+        }
+      }
+  }
 }
 
 void calc_angle() {
   float abs_kPa = C_kPa * boost_data.iat * boost_data.maf / boost_data.rpm;
   float intermediate;
-  if (boost_data.atm <= abs_kPa) {
-    intermediate = PSI_CONV_FACTOR * (abs_kPa - boost_data.atm);
-    boost_data.is_psi = true;
-  } else { // if I did the math right the "max(" is probably superfluous
-    intermediate = max(4.0 - ((float(boost_data.atm) - abs_kPa) / 25), 0.0);
-    boost_data.is_psi = false; //\ 100kPa/bar, 4x & inverted
-  }
+  boost_data.is_psi = boost_data.atm <= abs_kPa ? true : false;
+  Serial.println(abs_kPa - boost_data.atm); // send pressure data to pi for alt display and graphing
+  if (boost_data.is_psi) { intermediate = PSI_CONV_FACTOR * (abs_kPa - boost_data.atm); }
+  else { intermediate = max(4.0 - ((float(boost_data.atm) - abs_kPa) / 25), 0.0); }// 100kPa/bar, 4x & inverted
   boost_data.boost_angle = uint8_t(trunc(intermediate * PSI_SCALE));
   lv_scale_set_image_needle_value((boost_data.is_psi ? psi_scale : bar_scale), needle, boost_data.boost_angle);
 }
@@ -211,8 +197,6 @@ void make_screen_elements() {
   lv_obj_set_style_bg_opa(psi_scale, LV_OPA_COVER, 0);
   lv_obj_set_style_bg_color(psi_scale,lv_color_hex(0x051209),0);
   lv_obj_set_style_radius(psi_scale,240,0);
-  //lv_obj_set_style_radial_offset(psi_scale,100,LV_PART_INDICATOR);
-  //lv_obj_set_style_transform_scale(psi_scale, 200, LV_PART_INDICATOR);
   needle = lv_image_create(lv_screen_active());
   lv_image_set_src(needle,&NEEDLE1);
   lv_scale_set_image_needle_value(psi_scale,needle,boost_data.boost_angle);
